91b3ab8ad1a5b08825c3e184fbc7cb0154c9d3e7
SnurAulia <87569975+SnurAulia@users.noreply.github.com>
2022-03-27 18:44:30 +0700

    Delete Tugas Akhir directory

--- Tugas Akhir/delete_data_dist.php
+++
@@ -1,8 +0,0 @@
-<?php
-include"koneksi.php";
-mysqli_query($con,"delete from tbl_dist where id_dist = '$_GET[id]'");
-
-echo"<script language = 'JavaScript'> confirm('Data Berhasil Dihapus!');
-	document.location='index.php?page=data_dist'; </script>";
-
-?>
--- Tugas Akhir/delete_data_produk.php
+++
@@ -1,8 +0,0 @@
-<?php
-include"koneksi.php";
-mysqli_query($con,"delete from tbl_produk where id_buku = '$_GET[id]'");
-
-echo"<script language = 'JavaScript'> confirm('Data Berhasil Dihapus!');
-	document.location='index.php?page=data_produk'; </script>";
-
-?>
